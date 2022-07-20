14481e61f918ffa26d24012eb14097ebe9dd8451
HodanekHonza <98991609+HodanekHonza@users.noreply.github.com>
2022-07-20 18:07:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+
+
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+ 
+}
+console.log(generateTarget())
+
+const compareGuesses = (humanGuess, pcGuess, secretNumber) => {
+  const humanDifference = Math.abs(secretNumber - humanGuess)
+  const computerDifference = Math.abs(secretNumber - pcGuess)
+  if (humanDifference <= computerDifference) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(compareGuesses())
+
+const updateScore = (winner) => {
+  if (winner === "computer") {
+    computerScore++;
+  } else {
+    humanScore++;
+  }
+}
+
+const advanceRound = () => {
+  currentRoundNumber++;
+}
+
+console.log()
+console.log(updateScore())
+console.log()
+console.log()
